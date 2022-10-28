939e0f9f1b9cfe2880a91e7f8e4c9654f141e880
ahaaster <a.a.vanhaaster@protonmail.com>
2022-10-28 16:03:44 +0200

    added timezone setting functionality

--- date_to.py
+++ date_to.py
@@ -1,5 +1,5 @@
 """A handy python function to parse and convert to and between datetime.datetime, int, and string objects"""
-__version__ = "1.0b1"
+__version__ = "1.0"
 
 import math
 import datetime
@@ -7,26 +7,32 @@ from dateparser import parse
 
 
 DateTypes = str | int | float | datetime.date
-TIMESTAMP_DIGITS = 10
-DEFAULT_SETTINGS = {
-    "TIMEZONE": "UTC",
-    "PREFER_DAY_OF_MONTH": "first",
-    "RETURN_AS_TIMEZONE_AWARE": True,
-}
 ACCEPTED_STRINGS = {
     "str": ["str", "string", "text",],
     "date": ["datetime.date", "datetime", "date",],
     "int": ["int", "timestamp", "epoch", "unix", "float",],
 }
 
+TIMESTAMP_DIGITS = 10
+DEFAULT_SETTINGS = {
+    "TIMEZONE": "UTC",
+    "PREFER_DAY_OF_MONTH": "first",
+    "RETURN_AS_TIMEZONE_AWARE": True,
+}
 
-def date_to(your_date: DateTypes, /, end_type: DateTypes, *, parser_settings: dict = None) -> DateTypes:
+
+def date_to(your_date: DateTypes, /, end_type: DateTypes,  
+            timezone: str = None, *, parser_settings: dict = None,
+            ) -> DateTypes:
     """
     :param your_date: Requires to be either int, float, str, or datetime.date
     :param end_type: The type you want the date to be converted into
-    :return: converted time to specified Type[end_type] rounded to seconds and always in UTC
+    :param timezone: Three letter abbreviation (case insensitive) of desired output timezone. Default is "UTC"
+    :param parser_settings: dict of keyword arguments for overriding default dateparser.parse() settings
+    :return: converted time to specified Type[end_type] rounded to seconds
     """
 
+       
     if isinstance(end_type, str):
         if end_type.lower() in ACCEPTED_STRINGS["int"]:
             end_type = int
@@ -40,6 +46,8 @@ def date_to(your_date: DateTypes, /, end_type: DateTypes, *, parser_settings: di
                 f"Other accepted string representations of these types are: {ACCEPTED_STRINGS}"
             )
 
+    settings = _parse_settings(timezone, parser_settings)
+    
     # TODO: Add behaviour for float end_type support
     if not your_date:
         return your_date
@@ -47,11 +55,11 @@ def date_to(your_date: DateTypes, /, end_type: DateTypes, *, parser_settings: di
         your_date = _round_timestamp_to_seconds(your_date)
 
     if end_type == datetime.date:
-        your_date = _to_datetime(your_date, parser_settings)
+        your_date = _to_datetime(your_date, settings)
 
     elif end_type == int:
         if isinstance(your_date, str):
-            your_date = _string_date_to_timestamp(your_date, parser_settings)
+            your_date = _string_date_to_timestamp(your_date, settings)
         elif isinstance(your_date, datetime.date):
             your_date = _date_time_to_timestamp(your_date)
         elif isinstance(your_date, float):
@@ -60,7 +68,7 @@ def date_to(your_date: DateTypes, /, end_type: DateTypes, *, parser_settings: di
     elif end_type == str:
         if isinstance(your_date, str):
             # This operation completes a possibly incomplete query_string to seconds
-            your_date = _to_datetime(your_date, parser_settings)
+            your_date = _to_datetime(your_date, settings)
         elif isinstance(your_date, (int, float)):
             your_date = _to_datetime(your_date)
         your_date = your_date.isoformat()
@@ -68,13 +76,11 @@ def date_to(your_date: DateTypes, /, end_type: DateTypes, *, parser_settings: di
     return your_date
 
 
-def _to_datetime(_time: DateTypes, parser_settings: dict = None) -> datetime.date:
+def _to_datetime(_time: DateTypes, settings: dict = DEFAULT_SETTINGS) -> datetime.date:
     if not isinstance(_time, (int, float)):
         if isinstance(_time, str):
-            settings = parser_settings if parser_settings else DEFAULT_SETTINGS
             _time = parse(_time, settings=settings)
-            
-        _time = _date_time_to_timestamp(_time)
+        _time = _date_time_to_timestamp(_time) 
 
     return datetime.datetime.fromtimestamp(_time, tz=datetime.timezone.utc)
 
@@ -91,13 +97,21 @@ def _round_timestamp_to_seconds(_timestamp: int) -> int:  # Assumes positive num
 def _date_time_to_timestamp(_date_time) -> int:
     # pd.to_datetime uses pytz, can result in conflict
     # we're overriding the timezone class to avoid any conflict
-    # Issue:https://stackoverflow.com/questions/60736569/timestamp-subtraction-must-have-the-same-timezones-or-no-timezones-but-they-are
+    # Issue: https://stackoverflow.com/questions/60736569/timestamp-subtraction-must-have-the-same-timezones-or-no-timezones-but-they-are
     # TODO: test if this overriding causes non-UTC timezones to get borked
     _date_time = _date_time.replace(tzinfo=datetime.timezone.utc)
     unix_start = datetime.datetime.fromtimestamp(0, tz=datetime.timezone.utc)
     return int((_date_time - unix_start).total_seconds())
 
 
-def _string_date_to_timestamp(_date_string: str, parser_settings: dict = None) -> int:
-    date_time = _to_datetime(_date_string, parser_settings)
+def _string_date_to_timestamp(_date_string: str, settings: dict) -> int:
+    date_time = _to_datetime(_date_string, settings)
     return _date_time_to_timestamp(date_time)
+
+
+def _parse_settings(timezone: str = None, parser_settings: dict = None) -> dict:
+        settings = parser_settings if parser_settings else DEFAULT_SETTINGS
+        if timezone:
+            settings["TIMEZONE"] = timezone.upper()
+        return settings
+
