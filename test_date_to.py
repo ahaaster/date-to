b23ff3c0dbf8cdedfe32e35d095820905d703f50
ahaaster <a.a.vanhaaster@protonmail.com>
2022-10-30 21:43:51 +0100

    changed unittest to pytest

---
+++ test_date_to.py
@@ -0,0 +1,43 @@
+import pytest
+import datetime
+from date_to import date_to, DateInputError
+
+
+LOCAL_STR = "2001-09-11 17:20:52 EDT"
+TIME_STR = "2001-09-11T21:20:52+00:00"
+TIME_INT = 1000243252
+TIME_DATE = datetime.datetime(2001, 9, 11, 21, 20, 52, tzinfo=datetime.timezone.utc)
+
+
+@pytest.mark.parametrize("input_date, input_type, expected", [
+    (LOCAL_STR, str, TIME_STR),
+    (LOCAL_STR, int, TIME_INT),
+    (LOCAL_STR, float, TIME_INT),
+    (LOCAL_STR, datetime.date, TIME_DATE),
+    
+    (TIME_INT, "str", TIME_STR),
+    (TIME_INT, "int", TIME_INT),
+    (TIME_INT, "dt", TIME_DATE),
+    
+    (TIME_DATE, "text", TIME_STR),
+    (TIME_DATE, "timestamp", TIME_INT),
+    (TIME_DATE, "date", TIME_DATE)
+    
+])
+def test_basic_input_output(input_date, input_type, expected):
+    assert date_to(input_date, input_type) == expected
+
+
+def test_invalid_str_endtype():
+    with pytest.raises(KeyError):
+        date_to(LOCAL_STR, "Not a valid datetype")
+
+
+def test_invalid_int_input():
+    with pytest.raises(DateInputError):
+        date_to(-1, int)
+
+        
+def test_invalid_str_input():
+    with pytest.raises(DateInputError):
+        date_to("Een uur geleden")
