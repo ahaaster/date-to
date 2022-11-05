d93fe81970ccde18061449c5c4803049db2561ea
ahaaster <a.a.vanhaaster@protonmail.com>
2022-11-05 15:55:15 +0100

    added end_type test + shortened datetime to dt

--- test_date_to.py
+++ test_date_to.py
@@ -1,19 +1,18 @@
 import pytest
-import datetime
+import datetime as dt
 from date_to import date_to, DateInputError
 
 
 LOCAL_STR = "2001-09-11 17:20:52 EDT"
 TIME_STR = "2001-09-11T21:20:52+00:00"
 TIME_INT = 1000243252
-TIME_DATE = datetime.datetime(2001, 9, 11, 21, 20, 52, tzinfo=datetime.timezone.utc)
+TIME_DATE = dt.datetime(2001, 9, 11, 21, 20, 52, tzinfo=dt.timezone.utc)
 
 
 @pytest.mark.parametrize("input_date, input_type, expected", [
     (LOCAL_STR, str, TIME_STR),
     (LOCAL_STR, int, TIME_INT),
-    (LOCAL_STR, float, TIME_INT),
-    (LOCAL_STR, datetime.datetime, TIME_DATE),
+    (LOCAL_STR, dt.datetime, TIME_DATE),
     
     (TIME_INT, "str", TIME_STR),
     (TIME_INT, "int", TIME_INT),
@@ -23,8 +22,10 @@ TIME_DATE = datetime.datetime(2001, 9, 11, 21, 20, 52, tzinfo=datetime.timezone.
     (TIME_DATE, "timestamp", TIME_INT),
     (TIME_DATE, "date", TIME_DATE),
     
+    (LOCAL_STR, float, TIME_INT),
     (TIME_INT+0.2, "unix", TIME_INT),
     (TIME_INT*10**3, "epoch", TIME_INT),
+    (TIME_INT, dt.date, TIME_DATE)
 ])
 def test_basic_input_output(input_date, input_type, expected):
     assert date_to(input_date, input_type) == expected
@@ -33,7 +34,6 @@ def test_basic_input_output(input_date, input_type, expected):
 @pytest.mark.parametrize("input_date, input_type, tz, expected", [
     (LOCAL_STR, str, "JST", "2001-09-12T06:20:52+09:00"),
     (TIME_STR, str, "JST", "2001-09-12T06:20:52+09:00"),
-    # (LOCAL_STR, "datetime", "JST", "2001-09-12T06:20:52+09:00"),
 ])
 def test_timezone_conversion(input_date, input_type, tz, expected):
     assert date_to(input_date, input_type, tz) == expected
